81fe57467c3de4928c88e9d36e00288f15c027f3
Sushant Chandra Kalia <sushantchandrakalia@gmail.com>
2022-08-31 17:17:28 +0530

    Working_With_Variables

---
+++ Working_With_Variables/Printing.py
@@ -0,0 +1,19 @@
+# Print Function
+print("Hello World!")
+
+# Printing to separate line with  '\n' :
+print("Hello World!\nHello World!\nHello World!!!")
+
+# String Concatenation
+print("Hello" + "Sushant")  # No space between the two words
+print("Hello " + "Sushant")
+print("Hello" + " Sushant")
+print("Hello" + " " + "Sushant")
+
+# Python Input Function
+input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+# Python Variables
+name = input("What is your name?")
+print(name)
